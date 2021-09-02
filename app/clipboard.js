c2aea20c56bce90ed6bafb6a8711820a7dd712b2
Raúl Ruiz Bolivar <raul.ruiz.bolivar@gmail.com>
2021-09-02 17:34:14 +0200

    Quitando variable innecesaria

--- app/clipboard.js
+++ app/clipboard.js
@@ -1,4 +1,4 @@
 function copyClipboard(id){
     let elemento = document.getElementById(id);
-    let copiado = navigator.clipboard.writeText(elemento.id);
+    navigator.clipboard.writeText(elemento.id);
 }
